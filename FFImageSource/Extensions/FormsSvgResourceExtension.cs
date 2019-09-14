070dfb2fc9767f570346dde587b36ea1140130c6
Satoshi Nakamura <kamu@kamusoft-mini-2.local>
2019-09-14 12:33:18 +0900

    Add Extensions

---
+++ FFImageSource/Extensions/FormsSvgResourceExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+namespace FFImageLoading.Forms.Extensions
+{
+    [ContentProperty("Path")]
+    public class FormsSvgResourceExtension:IMarkupExtension<ImageSource>
+    {
+        public string Path { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Color FillColor { get; set; }
+
+        public ImageSource ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Path != null)
+            {
+                return FFSourceFactory.FromSvgPath(Path, Width, Height, FillColor);
+            }
+
+            return null;
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return (this as IMarkupExtension<ImageSource>).ProvideValue(serviceProvider);
+        }
+    }
+}
